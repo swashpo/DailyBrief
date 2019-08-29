7a231d515b1ade35e4103afff7ae2250b21bc164
Richard <54151422+swashpo@users.noreply.github.com>
2019-08-29 16:00:34 -0400

    Add files via upload
    Beginnings of window, menus

--- Main.java
+++ Main.java
@@ -1,6 +1,50 @@
+import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+
 public class Main {
 
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Scores Academy");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // menu stuff
+        JMenuBar menuBar;
+        JMenu menu, submenu;
+        JMenuItem menuItem, menuItem1;
+
+        menuBar = new JMenuBar();
+
+        menu = new JMenu("Options");
+        menu.setMnemonic(KeyEvent.VK_A);
+        menu.getAccessibleContext().setAccessibleDescription("Go here!");
+        menuBar.add(menu);
+
+        menuItem = new JMenuItem("Add/remove student", KeyEvent.VK_T);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke
+                (KeyEvent.VK_1, ActionEvent.SHIFT_MASK));
+
+        submenu = new JMenu("Current roster");
+        submenu.setMnemonic(KeyEvent.VK_S);
+
+        menuItem = new JMenuItem("Attendance");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2,
+                ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+        menuItem = new JMenuItem("Input Scores");
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2,
+                ActionEvent.ALT_MASK));
+        submenu.add(menuItem);
+
+
+    }
+}
 
         // bring up display with following tabs:
 
@@ -20,5 +64,3 @@ public class Main {
             // trends displays graphs of overall class scores, individual student trends
 
 
-    }
-}
